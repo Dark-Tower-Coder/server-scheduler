d3d478af47fd591a52ac8ef5f23ca50a8bb9916b
Dark-Tower-Coder <tower@posteo.de>
2021-02-04 22:41:38 +0100

    create project with ide

---
+++ src/main/java/de/towers_web/server_scheduler/ServerSchedulerApplication.java
@@ -0,0 +1,13 @@
+package de.towers_web.server_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/towers_web/server_scheduler/ServerSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.towers_web.server_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
